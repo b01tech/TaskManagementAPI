61738c508fcde4ed553183443bff8f03d1756944
b01tech <bruno01.tech@gmail.com>
2024-10-28 14:33:42 -0300

    feat: add TaskController for CRUD operations

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using TaskManagementAPI.Data;
+using TaskManagementAPI.Models;
+
+namespace TaskManagementAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class TasksController : ControllerBase
+{
+    private readonly TaskContext _context;
+    public TasksController(TaskContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskItem>>> GetAllTasks()
+    {
+        return await _context.Tasks.Take(100).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TaskItem>> GetTask(int id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+        return Ok(task);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TaskItem>> CreateTask(TaskItem task)
+    {
+        if (task == null)
+        {
+            return BadRequest();
+        }
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("GetTask", new { id = task.Id }, task);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateTask(int id, TaskItem task)
+    {
+        if (task.Id != id)
+        {
+            return BadRequest();
+        }
+        _context.Entry(task).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Tasks.Any(t => t.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(int id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+        _context.Tasks.Remove(task);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,22 +2,30 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using TaskManagementAPI.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<TaskContext>(option => 
-    option.UseSqlite(builder.Configuration.GetConnectionString("TaskDbConnetion")));
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+
+        builder.Services.AddDbContext<TaskContext>(option =>
+            option.UseSqlite(builder.Configuration.GetConnectionString("TaskDbConnetion")));
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();    
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
